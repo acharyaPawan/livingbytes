6f021ddb2b780c615c44293d02e76f69021e100c
Pawan Acharya <pawanacharya101>
2024-02-26 23:23:59 +0545

    SQL: Query data for tasks is ordered

--- src/server/api/routers/tasks.ts
+++ src/server/api/routers/tasks.ts
@@ -28,10 +28,12 @@ import { Category } from "@/types/types";
 
 export const taskRouter = createTRPCRouter({
     getCategorizedTasks: protectedProcedure.query(async () => {
+
+
       const sqlCommand = sql`SELECT
       c.id AS categoryId,
       c.title AS categoryName,
-      c.description as categoryDescription,
+      c.description AS categoryDescription,
       c.priority AS categoryPriority,
       c.labels AS categoryLabels,
       c.remark AS categoryRemark,
@@ -46,7 +48,7 @@ export const taskRouter = createTRPCRouter({
           'priority', t.priority,
           'locked', t.locked,
           'flexible', t.flexible,
-          'priorityLabel',t.priority_label,
+          'priorityLabel', t.priority_label,
           'createdOn', t.created_on,
           'expiresOn', t.expires_on,
           'completedOn', t.completed_on,
@@ -57,36 +59,36 @@ export const taskRouter = createTRPCRouter({
             SELECT
               jsonb_agg(
                 jsonb_build_object(
-            'id', st.id,
-            'taskId', st.task_id,
-            'title', st.title,
-            'description', st.description,
-            'status', st.status,
-            'priority', st.priority,
-            'locked', st.locked,
-            'flexible', st.flexible,
-            'priorityLabel',st.priority_label,
-            'createdOn', st.created_on,
-            'expiresOn', st.expires_on,
-            'completedOn', st.completed_on,
-            'viewAs', st.view_as,
-            'specialLabels', st.special_labels,
-            'remark', st.remark,
-            'tracker', (
-              SELECT
-              jsonb_agg(
-                jsonb_build_object(
-                  'id', tr.id,
-                  'title', tr.title,
-                  'frequency', tr.frequency,
-                  'createdOn', tr.created_on
-                )
-              )
-              FROM
-                trackers tr
-              WHERE
-                tr.task_id = st.id
-            )
+                  'id', st.id,
+                  'taskId', st.task_id,
+                  'title', st.title,
+                  'description', st.description,
+                  'status', st.status,
+                  'priority', st.priority,
+                  'locked', st.locked,
+                  'flexible', st.flexible,
+                  'priorityLabel', st.priority_label,
+                  'createdOn', st.created_on,
+                  'expiresOn', st.expires_on,
+                  'completedOn', st.completed_on,
+                  'viewAs', st.view_as,
+                  'specialLabels', st.special_labels,
+                  'remark', st.remark,
+                  'tracker', (
+                    SELECT
+                      jsonb_agg(
+                        jsonb_build_object(
+                          'id', tr.id,
+                          'title', tr.title,
+                          'frequency', tr.frequency,
+                          'createdOn', tr.created_on
+                        )
+                      )
+                    FROM
+                      trackers tr
+                    WHERE
+                      tr.task_id = st.id
+                  )
                 )
               )
             FROM
@@ -96,33 +98,33 @@ export const taskRouter = createTRPCRouter({
           ),
           'tracker', (
             SELECT
-            jsonb_agg(
-              jsonb_build_object(
-                'id', tr.id,
-                'title', tr.title,
-                'frequency', tr.frequency,
-                'createdOn', tr.created_on
+              jsonb_agg(
+                jsonb_build_object(
+                  'id', tr.id,
+                  'title', tr.title,
+                  'frequency', tr.frequency,
+                  'createdOn', tr.created_on
+                )
               )
-            )
             FROM
               trackers tr
             WHERE
               tr.task_id = t.id AND tr.tracked
           )
-        )
+        ) ORDER BY t.view_as ASC, t.created_on DESC, t.priority DESC
       ) AS tasks
     FROM
       categories c
     JOIN
       tasks t ON c.id = t.category_id
     GROUP BY
-      c.id, c.title
+      c.id, c.title, c.description, c.priority, c.labels, c.remark, c.created_on
     ORDER BY
-      c.priority;
+      c.priority;       
     `
     const res = await db.execute(sqlCommand)
     const resr: Category[] = res.rows as unknown as Category[]
-    // console.log('resr is ', resr)
+    console.log('resr is ', resr[0]?.tasks[0])
     return resr as Category[]        
 })
 })
